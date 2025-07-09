1aba7e90d909cb55642ddb26c6484dab782e0a2c
oracletemple <zdc241216@outlook.com>
2025-07-09 12:52:53 +0300

    Create G_dynamic-loader.js

---
+++ G_dynamic-loader.js
@@ -0,0 +1,37 @@
+// G_dynamic-loader.js - v1.0.0
+
+const { getGptAnalysis } = require("./G_gpt-analysis");
+const { getTarotSummary } = require("./G_tarot-summary");
+const { getJournalPrompt } = require("./G_journal-prompt");
+const { getShadowMessage } = require("./G_shadow-message");
+const { getSoulArchetype } = require("./G_soul-archetype");
+const { getHigherSelfMessage } = require("./G_higher-self");
+const { getCosmicAlignment } = require("./G_cosmic-alignment");
+const { getOracleCard } = require("./G_oracle-card");
+const { getPastLifeEcho } = require("./G_pastlife");
+const { getSoulPurpose } = require("./G_soul-purpose");
+const { getKarmicCycle } = require("./G_karma");
+const { getEnergyReading } = require("./G_energy-reading");
+const { getDivineTiming } = require("./G_divine-timing");
+const { getSacredSymbol } = require("./G_sacred-symbol");
+const { getSpiritMessage } = require("./G_spirit-message");
+const { getMirrorMessage } = require("./G_mirror-message");
+
+module.exports = {
+  getGptAnalysis,
+  getTarotSummary,
+  getJournalPrompt,
+  getShadowMessage,
+  getSoulArchetype,
+  getHigherSelfMessage,
+  getCosmicAlignment,
+  getOracleCard,
+  getPastLifeEcho,
+  getSoulPurpose,
+  getKarmicCycle,
+  getEnergyReading,
+  getDivineTiming,
+  getSacredSymbol,
+  getSpiritMessage,
+  getMirrorMessage,
+};
