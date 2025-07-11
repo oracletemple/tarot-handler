750cb299782135716b887ed77c5bbc7ce12a1b31
oracletemple <zdc241216@outlook.com>
2025-07-11 20:11:54 +0300

    Update G_card-data.js

--- G_card-data.js
+++ G_card-data.js
@@ -3,67 +3,67 @@ module.exports = [
     "id": 0,
     "name": "The Fool",
     "meaning": "The symbolic meaning of The Fool.",
-    "image": "The Fool.jpg"
+    "image": "The_Fool.jpg"
   },
   {
     "id": 1,
     "name": "The Magician",
     "meaning": "The symbolic meaning of The Magician.",
-    "image": "The Magician.jpg"
+    "image": "The_Magician.jpg"
   },
   {
     "id": 2,
     "name": "The High Priestess",
     "meaning": "The symbolic meaning of The High Priestess.",
-    "image": "The High Priestess.jpg"
+    "image": "The_High_Priestess.jpg"
   },
   {
     "id": 3,
     "name": "The Empress",
     "meaning": "The symbolic meaning of The Empress.",
-    "image": "The Empress.jpg"
+    "image": "The_Empress.jpg"
   },
   {
     "id": 4,
     "name": "The Emperor",
     "meaning": "The symbolic meaning of The Emperor.",
-    "image": "The Emperor.jpg"
+    "image": "The_Emperor.jpg"
   },
   {
     "id": 5,
     "name": "The Hierophant",
     "meaning": "The symbolic meaning of The Hierophant.",
-    "image": "The Hierophant.jpg"
+    "image": "The_Hierophant.jpg"
   },
   {
     "id": 6,
     "name": "The Lovers",
     "meaning": "The symbolic meaning of The Lovers.",
-    "image": "The Lovers.jpg"
+    "image": "The_Lovers.jpg"
   },
   {
     "id": 7,
     "name": "The Chariot",
     "meaning": "The symbolic meaning of The Chariot.",
-    "image": "The Chariot.jpg"
+    "image": "The_Chariot.jpg"
   },
   {
     "id": 8,
     "name": "Strength",
     "meaning": "The symbolic meaning of Strength.",
-    "image": "The Justice.jpg"
+    "image": "The_Justice.jpg"
   },
   {
     "id": 9,
     "name": "The Hermit",
     "meaning": "The symbolic meaning of The Hermit.",
-    "image": "The Hermit.jpg"
+    "image": "The_Hermit.jpg"
   },
   {
     "id": 10,
     "name": "Wheel of Fortune",
     "meaning": "The symbolic meaning of Wheel of Fortune.",
-    "image": "Wheel of Fortune.jpg"
+    "image": "Wheel_of_Fortune.jpg"
   },
   {
     "id": 11,
@@ -75,7 +75,7 @@ module.exports = [
     "id": 12,
     "name": "The Hanged Man",
     "meaning": "The symbolic meaning of The Hanged Man.",
-    "image": "The Hanged_Man.jpg"
+    "image": "The_Hanged_Man.jpg"
   },
   {
     "id": 13,
@@ -93,31 +93,31 @@ module.exports = [
     "id": 15,
     "name": "The Devil",
     "meaning": "The symbolic meaning of The Devil.",
-    "image": "The Devil.jpg"
+    "image": "The_Devil.jpg"
   },
   {
     "id": 16,
     "name": "The Tower",
     "meaning": "The symbolic meaning of The Tower.",
-    "image": "The Tower.jpg"
+    "image": "The_Tower.jpg"
   },
   {
     "id": 17,
     "name": "The Star",
     "meaning": "The symbolic meaning of The Star.",
-    "image": "The Star.jpg"
+    "image": "The_Star.jpg"
   },
   {
     "id": 18,
     "name": "The Moon",
     "meaning": "The symbolic meaning of The Moon.",
-    "image": "The Moon.jpg"
+    "image": "The_Moon.jpg"
   },
   {
     "id": 19,
     "name": "The Sun",
     "meaning": "The symbolic meaning of The Sun.",
-    "image": "The Sun.jpg"
+    "image": "The_Sun.jpg"
   },
   {
     "id": 20,
@@ -129,342 +129,342 @@ module.exports = [
     "id": 21,
     "name": "The World",
     "meaning": "The symbolic meaning of The World.",
-    "image": "The World.jpg"
+    "image": "The_World.jpg"
   },
   {
     "id": 22,
     "name": "Ace of Wands",
     "meaning": "The symbolic meaning of Ace of Wands.",
-    "image": "Ace of Wands.jpg"
+    "image": "Ace_of_Wands.jpg"
   },
   {
     "id": 23,
     "name": "Two of Wands",
     "meaning": "The symbolic meaning of Two of Wands.",
-    "image": "Two of Wands.jpg"
+    "image": "Two_of_Wands.jpg"
   },
   {
     "id": 24,
     "name": "Three of Wands",
     "meaning": "The symbolic meaning of Three of Wands.",
-    "image": "Three of Wands.jpg"
+    "image": "Three_of_Wands.jpg"
   },
   {
     "id": 25,
     "name": "Four of Wands",
     "meaning": "The symbolic meaning of Four of Wands.",
-    "image": "Four of Wands.jpg"
+    "image": "Four_of_Wands.jpg"
   },
   {
     "id": 26,
     "name": "Five of Wands",
     "meaning": "The symbolic meaning of Five of Wands.",
-    "image": "Five of Wands.jpg"
+    "image": "Five_of_Wands.jpg"
   },
   {
     "id": 27,
     "name": "Six of Wands",
     "meaning": "The symbolic meaning of Six of Wands.",
-    "image": "Six of Wands.jpg"
+    "image": "Six_of_Wands.jpg"
   },
   {
     "id": 28,
     "name": "Seven of Wands",
     "meaning": "The symbolic meaning of Seven of Wands.",
-    "image": "Seven of Wands.jpg"
+    "image": "Seven_of_Wands.jpg"
   },
   {
     "id": 29,
     "name": "Eight of Wands",
     "meaning": "The symbolic meaning of Eight of Wands.",
-    "image": "Eight of Wands.jpg"
+    "image": "Eight_of_Wands.jpg"
   },
   {
     "id": 30,
     "name": "Nine of Wands",
     "meaning": "The symbolic meaning of Nine of Wands.",
-    "image": "Nine of Wands.jpg"
+    "image": "Nine_of_Wands.jpg"
   },
   {
     "id": 31,
     "name": "Ten of Wands",
     "meaning": "The symbolic meaning of Ten of Wands.",
-    "image": "Ten of Wands.jpg"
+    "image": "Ten_of_Wands.jpg"
   },
   {
     "id": 32,
     "name": "Page of Wands",
     "meaning": "The symbolic meaning of Page of Wands.",
-    "image": "Page of Wands.jpg"
+    "image": "Page_of_Wands.jpg"
   },
   {
     "id": 33,
     "name": "Knight of Wands",
     "meaning": "The symbolic meaning of Knight of Wands.",
-    "image": "Knight of Wands.jpg"
+    "image": "Knight_of_Wands.jpg"
   },
   {
     "id": 34,
     "name": "Queen of Wands",
     "meaning": "The symbolic meaning of Queen of Wands.",
-    "image": "Queen of Wands.jpg"
+    "image": "Queen_of_Wands.jpg"
   },
   {
     "id": 35,
     "name": "King of Wands",
     "meaning": "The symbolic meaning of King of Wands.",
-    "image": "King of Wands.jpg"
+    "image": "King_of_Wands.jpg"
   },
   {
     "id": 36,
     "name": "Ace of Cups",
     "meaning": "The symbolic meaning of Ace of Cups.",
-    "image": "Ace of Cups.jpg"
+    "image": "Ace_of_Cups.jpg"
   },
   {
     "id": 37,
     "name": "Two of Cups",
     "meaning": "The symbolic meaning of Two of Cups.",
-    "image": "Two of Cups.jpg"
+    "image": "Two_of_Cups.jpg"
   },
   {
     "id": 38,
     "name": "Three of Cups",
     "meaning": "The symbolic meaning of Three of Cups.",
-    "image": "Three of Cups.jpg"
+    "image": "Three_of_Cups.jpg"
   },
   {
     "id": 39,
     "name": "Four of Cups",
     "meaning": "The symbolic meaning of Four of Cups.",
-    "image": "Four of Cups.jpg"
+    "image": "Four_of_Cups.jpg"
   },
   {
     "id": 40,
     "name": "Five of Cups",
     "meaning": "The symbolic meaning of Five of Cups.",
-    "image": "Five of Cups.jpg"
+    "image": "Five_of_Cups.jpg"
   },
   {
     "id": 41,
     "name": "Six of Cups",
     "meaning": "The symbolic meaning of Six of Cups.",
-    "image": "Six of Cups.jpg"
+    "image": "Six_of_Cups.jpg"
   },
   {
     "id": 42,
     "name": "Seven of Cups",
     "meaning": "The symbolic meaning of Seven of Cups.",
-    "image": "Seven of Cups.jpg"
+    "image": "Seven_of_Cups.jpg"
   },
   {
     "id": 43,
     "name": "Eight of Cups",
     "meaning": "The symbolic meaning of Eight of Cups.",
-    "image": "Eight of Cups.jpg"
+    "image": "Eight_of_Cups.jpg"
   },
   {
     "id": 44,
     "name": "Nine of Cups",
     "meaning": "The symbolic meaning of Nine of Cups.",
-    "image": "Nine of Cups"
+    "image": "Nine_of_Cups"
   },
   {
     "id": 45,
     "name": "Ten of Cups",
     "meaning": "The symbolic meaning of Ten of Cups.",
-    "image": "Ten of Cups.jpg"
+    "image": "Ten_of_Cups.jpg"
   },
   {
     "id": 46,
     "name": "Page of Cups",
     "meaning": "The symbolic meaning of Page of Cups.",
-    "image": "Page of Cups.jpg"
+    "image": "Page_of_Cups.jpg"
   },
   {
     "id": 47,
     "name": "Knight of Cups",
     "meaning": "The symbolic meaning of Knight of Cups.",
-    "image": "Knight of Cups.jpg"
+    "image": "Knight_of_Cups.jpg"
   },
   {
     "id": 48,
     "name": "Queen of Cups",
     "meaning": "The symbolic meaning of Queen of Cups.",
-    "image": "Queen of Cups.jpg"
+    "image": "Queen_of_Cups.jpg"
   },
   {
     "id": 49,
     "name": "King of Cups",
     "meaning": "The symbolic meaning of King of Cups.",
-    "image": "King of Cups.jpg"
+    "image": "King_of_Cups.jpg"
   },
   {
     "id": 50,
     "name": "Ace of Swords",
     "meaning": "The symbolic meaning of Ace of Swords.",
-    "image": "Ace of Swords.jpg"
+    "image": "Ace_of_Swords.jpg"
   },
   {
     "id": 51,
     "name": "Two of Swords",
     "meaning": "The symbolic meaning of Two of Swords.",
-    "image": "Two of Swords.jpg"
+    "image": "Two_of_Swords.jpg"
   },
   {
     "id": 52,
     "name": "Three of Swords",
     "meaning": "The symbolic meaning of Three of Swords.",
-    "image": "Three of Swords.jpg"
+    "image": "Three_of_Swords.jpg"
   },
   {
     "id": 53,
     "name": "Four of Swords",
     "meaning": "The symbolic meaning of Four of Swords.",
-    "image": "Four of Swords.jpg"
+    "image": "Four_of_Swords.jpg"
   },
   {
     "id": 54,
     "name": "Five of Swords",
     "meaning": "The symbolic meaning of Five of Swords.",
-    "image": "Five of Swords.jpg"
+    "image": "Five_of_Swords.jpg"
   },
   {
     "id": 55,
     "name": "Six of Swords",
     "meaning": "The symbolic meaning of Six of Swords.",
-    "image": "Six of Swords.jpg"
+    "image": "Six_of_Swords.jpg"
   },
   {
     "id": 56,
     "name": "Seven of Swords",
     "meaning": "The symbolic meaning of Seven of Swords.",
-    "image": "Seven of Swords.jpg"
+    "image": "Seven_of_Swords.jpg"
   },
   {
     "id": 57,
     "name": "Eight of Swords",
     "meaning": "The symbolic meaning of Eight of Swords.",
-    "image": "Eight of Swords.jpg"
+    "image": "Eight_of_Swords.jpg"
   },
   {
     "id": 58,
     "name": "Nine of Swords",
     "meaning": "The symbolic meaning of Nine of Swords.",
-    "image": "Nine of Swords.jpg"
+    "image": "Nine_of_Swords.jpg"
   },
   {
     "id": 59,
     "name": "Ten of Swords",
     "meaning": "The symbolic meaning of Ten of Swords.",
-    "image": "Ten of Swords.jpg"
+    "image": "Ten_of_Swords.jpg"
   },
   {
     "id": 60,
     "name": "Page of Swords",
     "meaning": "The symbolic meaning of Page of Swords.",
-    "image": "Page of Swords.jpg"
+    "image": "Page_of_Swords.jpg"
   },
   {
     "id": 61,
     "name": "Knight of Swords",
     "meaning": "The symbolic meaning of Knight of Swords.",
-    "image": "Knight of Swords.jpg"
+    "image": "Knight_of_Swords.jpg"
   },
   {
     "id": 62,
     "name": "Queen of Swords",
     "meaning": "The symbolic meaning of Queen of Swords.",
-    "image": "Queen of Swords.jpg"
+    "image": "Queen_of_Swords.jpg"
   },
   {
     "id": 63,
     "name": "King of Swords",
     "meaning": "The symbolic meaning of King of Swords.",
-    "image": "King of Swords.jpg"
+    "image": "King_of_Swords.jpg"
   },
   {
     "id": 64,
     "name": "Ace of Pentacles",
     "meaning": "The symbolic meaning of Ace of Pentacles.",
-    "image": "Ace of Pentacles.jpg"
+    "image": "Ace_of_Pentacles.jpg"
   },
   {
     "id": 65,
     "name": "Two of Pentacles",
     "meaning": "The symbolic meaning of Two of Pentacles.",
-    "image": "Two of Pentacles.jpg"
+    "image": "Two_of_Pentacles.jpg"
   },
   {
     "id": 66,
     "name": "Three of Pentacles",
     "meaning": "The symbolic meaning of Three of Pentacles.",
-    "image": "Three of Pentacles.jpg"
+    "image": "Three_of_Pentacles.jpg"
   },
   {
     "id": 67,
     "name": "Four of Pentacles",
     "meaning": "The symbolic meaning of Four of Pentacles.",
-    "image": "Four of Pentacles.jpg"
+    "image": "Four_of_Pentacles.jpg"
   },
   {
     "id": 68,
     "name": "Five of Pentacles",
     "meaning": "The symbolic meaning of Five of Pentacles.",
-    "image": "Five of Pentacles.jpg"
+    "image": "Five_of_Pentacles.jpg"
   },
   {
     "id": 69,
     "name": "Six of Pentacles",
     "meaning": "The symbolic meaning of Six of Pentacles.",
-    "image": "Six of Pentacles.jpg"
+    "image": "Six_of_Pentacles.jpg"
   },
   {
     "id": 70,
     "name": "Seven of Pentacles",
     "meaning": "The symbolic meaning of Seven of Pentacles.",
-    "image": "Seven of Pentacles.jpg"
+    "image": "Seven_of_Pentacles.jpg"
   },
   {
     "id": 71,
     "name": "Eight of Pentacles",
     "meaning": "The symbolic meaning of Eight of Pentacles.",
-    "image": "Eight of Pentacles.jpg"
+    "image": "Eight_of_Pentacles.jpg"
   },
   {
     "id": 72,
     "name": "Nine of Pentacles",
     "meaning": "The symbolic meaning of Nine of Pentacles.",
-    "image": "Nine of Pentacles.jpg"
+    "image": "Nine_of_Pentacles.jpg"
   },
   {
     "id": 73,
     "name": "Ten of Pentacles",
     "meaning": "The symbolic meaning of Ten of Pentacles.",
-    "image": "Ten of Pentacles.jpg"
+    "image": "Ten_of_Pentacles.jpg"
   },
   {
     "id": 74,
     "name": "Page of Pentacles",
     "meaning": "The symbolic meaning of Page of Pentacles.",
-    "image": "Page of Pentacles.jpg"
+    "image": "Page_of_Pentacles.jpg"
   },
   {
     "id": 75,
     "name": "Knight of Pentacles",
     "meaning": "The symbolic meaning of Knight of Pentacles.",
-    "image": "Knight of Pentacles.jpg"
+    "image": "Knight_of_Pentacles.jpg"
   },
   {
     "id": 76,
     "name": "Queen of Pentacles",
     "meaning": "The symbolic meaning of Queen of Pentacles.",
-    "image": "Queen of Pentacles.jpg"
+    "image": "Queen_of_Pentacles.jpg"
   },
   {
     "id": 77,
     "name": "King of Pentacles",
     "meaning": "The symbolic meaning of King of Pentacles.",
-    "image": "King of Pentacles.jpg"
+    "image": "King_of_Pentacles.jpg"
   }
 ];
